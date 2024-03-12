2cf0d96f18f4dd6184668f3f1a9340a7e06cc10f
Iacob Razvan <91972381+iacobrazvan0@users.noreply.github.com>
2024-03-12 14:03:48 +0200

    update 1.2

--- JavaScript/Pagina_de_start.js
+++ JavaScript/Pagina_de_start.js
@@ -26,28 +26,33 @@ var latestDiscussions = [
   // Apelăm funcția pentru afișarea inițială a discuțiilor
   showLatestDiscussions();
   
-  // script.js
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Schimbă imaginea la fiecare 2 secunde (2000 milisecunde)
-}
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Apelăm funcția pentru a începe slideshow-ul
+  var slideIndex = 0;
   showSlides();
-});
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 10000); // Schimbă imaginea la fiecare 2 secunde (2000 milisecunde)
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Apelăm funcția pentru a începe slideshow-ul
+    showSlides();
+  });
+  
+  function toggleMenu() {
+    var navMenu = document.querySelector('nav');
+    navMenu.classList.toggle('nav-open');
+  }
+  
